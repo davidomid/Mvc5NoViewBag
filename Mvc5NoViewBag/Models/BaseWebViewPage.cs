f42762e66536e007e8cd91c27dca32325c7a359d
David Omid <davidomid@hotmail.com>
2017-04-01 19:10:49 +0100

    Added BaseWebViewPage, edited the Views/Web.config file so this class is used instead of WebViewPage, removed ViewBag

---
+++ Mvc5NoViewBag/Models/BaseWebViewPage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Mvc5NoViewBag.Models
+{
+    public abstract class BaseWebViewPage<T> : WebViewPage<T>
+    {
+        [Obsolete("Thought you could get away with using this in a Razor view? Nice try, sucker :)", true)]
+        public new dynamic ViewBag { get; set; }
+    }
+
+    public abstract class BaseWebViewPage : BaseWebViewPage<dynamic>
+    {
+
+    }
+}
--- Mvc5NoViewBag/Models/ContactPageViewModel.cs
+++ Mvc5NoViewBag/Models/ContactPageViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace Mvc5NoViewBag.Models
+﻿namespace Mvc5NoViewBag.Models
 {
     public class ContactPageViewModel
     {
