5f7debe1954347b1942a7b224d5c8702762a52cd
David Omid <davidomid@hotmail.com>
2017-04-01 18:19:58 +0100

    Added a default ASP.NET MVC5 project

---
+++ Mvc5NoViewBag/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mvc5NoViewBag.Startup))]
+namespace Mvc5NoViewBag
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
